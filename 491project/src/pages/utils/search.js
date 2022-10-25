c46d31257674e3453ae15f364d33caa320763586
calmakris <calmakris@csu.fullerton.edu>
2022-10-24 21:17:57 -0700

    Updated to use axios

--- 491project/src/pages/utils/search.js
+++ 491project/src/pages/utils/search.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const GEOCODE_URL = 'https://api.openweathermap.org/geo/1.0/direct?';
 const GEOCODEREV_URL = 'https://api.openweathermap.org/geo/1.0/reverse?';
 
@@ -5,30 +7,23 @@ let data = require('../../keys.json');
 const OpenWeatherAPI_KEY = data[0]['key'];
 
 export const getGeoCodeData = async (cityName) => {
-    try {
-        const geoCodeURL = `${GEOCODE_URL}q=${cityName}&limit=5&appid=${OpenWeatherAPI_KEY}`;
-        const geoCodeResponse = await fetch(geoCodeURL);
-        var geoCodeResult = await geoCodeResponse.json();
+	try {
+		const response = await axios.get(`${GEOCODE_URL}q=${cityName}&limit=5&appid=${OpenWeatherAPI_KEY}`);
+		return response.data;
+	} catch (err) {
+		console.log(`ERROR: ${err.message}`);
+	}
 
-        let geoDataArr = [];
-        for (var i = 0; i < geoCodeResult.length; i++) {
-            geoDataArr.push(geoCodeResult[i]);
-        }
-
-        return geoDataArr;
-    }  catch (err) {
-        console.log(`ERROR: ${err.message}`);
-    }
+	return null;
 }
 
 export const getGeoReverseCodeData = async (location) => {
-    try {
-        const geoCodeURL = `${GEOCODEREV_URL}lat=${location.lat}&lon=${location.lon}&limit=1&appid=${OpenWeatherAPI_KEY}`;
-        const geoCodeResponse = await fetch(geoCodeURL);
-        var geoCodeResult = await geoCodeResponse.json();
+	try {
+		const response = await axios.get(`${GEOCODEREV_URL}lat=${location.lat}&lon=${location.lon}&limit=1&appid=${OpenWeatherAPI_KEY}`);
+		return response.data[0];
+	} catch (err) {
+		console.log(`ERROR: ${err.message}`);
+	}
 
-        return geoCodeResult[0];
-    } catch (err) {
-        console.log(`ERROR: ${err.message}`);
-    }
+	return null;
 }
