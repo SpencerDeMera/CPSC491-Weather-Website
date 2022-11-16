936f5bbe6ef92ec45f79126a8aa401ed5e8c0919
SpencerDeMera <spencer.demera@csu.fullerton.edu>
2022-11-15 19:21:45 -0800

    Fixed text wrapping on search results

--- 491project/src/pages/components/resultarea.js
+++ 491project/src/pages/components/resultarea.js
@@ -26,18 +26,19 @@ export default function ResultArea({
   return (
     <div className="results-area">
       {searchResults && searchResults.map((location, index) => 
-          <div
-            className="search-results"
-            key={index}
-            onClick={() => handleSelect(index)}
-          >
-            <a className="locationName">{location.name}</a>
-            {location.state &&
-              <a className="locationSub">{location.state}, {location.country}</a>
-            }
-            {!location.state &&
-              <a className="locationSub">{location.country}</a>
-            }
+          <div className="search-results" key={index}>
+            <div
+              className="search-results-inner"
+              onClick={() => handleSelect(index)}
+            >
+              <a className="locationName">{location.name}</a>
+              {location.state &&
+                <a className="locationSub">{location.state}, {location.country}</a>
+              }
+              {!location.state &&
+                <a className="locationSub">{location.country}</a>
+              }
+            </div>
           </div>
       )}
       {/* handle if no search results */}
